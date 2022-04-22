662d8b45372c09b3002c902b4d10ad92879c1ef0
Savin Osuka <osukasavin2001@gmail.com>
2022-04-22 21:08:41 +0300

    Test deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const jsonServer = require("json-server");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-const router = jsonServer.router('./database.json')
+const router = jsonServer.router("./database.json");
 
 const server = jsonServer.create();
 const userdb = JSON.parse(fs.readFileSync("./users.json", "utf-8"));
@@ -20,25 +20,30 @@ const createToken = (payload) => {
     return jwt.sign(payload, SECRET_KEY, { expiresIn });
 };
 
-// Checking if user is logged in
-const isLoginAuthenticated = ({ email, password }) => {
+// Checking if customer is logged in
+const isCustomerLoginAuthenticated = ({ email, password }) => {
     return (
-        userdb.users.findIndex(
-            (user) => user.email === email && user.password === password
+        userdb.customers.findIndex(
+            (customer) =>
+                customer.email === email && customer.password === password
         ) !== -1
     );
 };
-const isRegisterAuthenticated = ({ email}) => {
-    return userdb.users.findIndex((user) => user.email === email) !== -1;
+// Checking if customer is logged in
+const isCustomerRegisterAuthenticated = ({ email }) => {
+    return (
+        userdb.customers.findIndex((customer) => customer.email === email) !==
+        -1
+    );
 };
 
 // Registration section
-server.post("/api/auth/register", (req, res) => {
-    const {  firstName, secondName, phone, email, userType, password } =
+server.post("/api/auth/register/customer", (req, res) => {
+    const { firstName, secondName, phone, email, userType, password } =
         req.body;
 
     // Checking if user exists
-    if (isRegisterAuthenticated({ email})) {
+    if (isCustomerRegisterAuthenticated({ email })) {
         const status = 401;
         const message = "Email already exist";
         res.status(status).json({ status, message });
@@ -55,8 +60,8 @@ server.post("/api/auth/register", (req, res) => {
         }
 
         data = JSON.parse(data.toString());
-        let last_item_id = data.users[data.users.length - 1].id;
-        data.users.push({
+        let last_item_id = data.customers[data.customers.length - 1].id;
+        data.customers.push({
             id: last_item_id + 1,
             firstName: firstName,
             secondName: secondName,
@@ -83,11 +88,11 @@ server.post("/api/auth/register", (req, res) => {
 });
 
 // Login section
-server.post("/api/auth/login", (req, res) => {
+server.post("/api/auth/login/customer", (req, res) => {
     const { email, password } = req.body;
 
     // Checking if email and password are correct
-    if (!isLoginAuthenticated({ email, password })) {
+    if (!isCustomerLoginAuthenticated({ email, password })) {
         const status = 401;
         const message = "Incorrect email or password";
         res.status(status).json({ status, message });
@@ -97,7 +102,176 @@ server.post("/api/auth/login", (req, res) => {
     res.status(200).json({ access_token });
 });
 
-server.use(router)
+
+
+// Checking if vendor is logged in
+const isVendorLoginAuthenticated = ({ email, password }) => {
+    return (
+        userdb.vendors.findIndex(
+            (vendor) =>
+            vendor.email === email && vendor.password === password
+        ) !== -1
+    );
+};
+// Checking if vendor is logged in
+const isVendorRegisterAuthenticated = ({ email }) => {
+    return (
+        userdb.vendors.findIndex((vendor) => vendor.email === email) !==
+        -1
+    );
+};
+
+// Vendor Registration section
+server.post("/api/auth/register/vendor", (req, res) => {
+    const { firstName, secondName, phone, email, userType, password } =
+        req.body;
+
+    // Checking if user exists
+    if (isVendorRegisterAuthenticated({ email })) {
+        const status = 401;
+        const message = "Email already exist";
+        res.status(status).json({ status, message });
+        return;
+    }
+
+    // Creating new user
+    fs.readFile("./users.json", (err, data) => {
+        if (err) {
+            const status = 401;
+            const message = err;
+            res.status(status).json({ status, message });
+            return;
+        }
+
+        data = JSON.parse(data.toString());
+        let last_item_id = data.vendors[data.vendors.length - 1].id;
+        data.vendors.push({
+            id: last_item_id + 1,
+            firstName: firstName,
+            secondName: secondName,
+            phone: phone,
+            email: email,
+            userType: userType,
+            password: password,
+        });
+        let writeData = fs.writeFile(
+            "./users.json",
+            JSON.stringify(data),
+            (err, result) => {
+                if (err) {
+                    const status = 401;
+                    const message = err;
+                    res.status(status).json({ status, message });
+                    return;
+                }
+            }
+        );
+    });
+    const access_token = createToken({ email, password });
+    res.status(200).json({ access_token });
+});
+
+// Vendor Login section
+server.post("/api/auth/login/vendor", (req, res) => {
+    const { email, password } = req.body;
+
+    // Checking if email and password are correct
+    if (!isVendorLoginAuthenticated({ email, password })) {
+        const status = 401;
+        const message = "Incorrect email or password";
+        res.status(status).json({ status, message });
+        return;
+    }
+    const access_token = createToken({ email, password });
+    res.status(200).json({ access_token });
+});
+
+
+// Checking if guest is logged in
+const isGuestLoginAuthenticated = ({ email, password }) => {
+    return (
+        userdb.guests.findIndex(
+            (guest) =>
+            guest.email === email && guest.password === password
+        ) !== -1
+    );
+};
+// Checking if guest is logged in
+const isGuestRegisterAuthenticated = ({ email }) => {
+    return (
+        userdb.guests.findIndex((guest) => guest.email === email) !==
+        -1
+    );
+};
+
+// Vendor Registration section
+server.post("/api/auth/register/guest", (req, res) => {
+    const { firstName, secondName, phone, email, userType, password } =
+        req.body;
+
+    // Checking if user exists
+    if (isGuestRegisterAuthenticated({ email })) {
+        const status = 401;
+        const message = "Email already exist";
+        res.status(status).json({ status, message });
+        return;
+    }
+
+    // Creating new user
+    fs.readFile("./users.json", (err, data) => {
+        if (err) {
+            const status = 401;
+            const message = err;
+            res.status(status).json({ status, message });
+            return;
+        }
+
+        data = JSON.parse(data.toString());
+        let last_item_id = data.guests[data.guests.length - 1].id;
+        data.guests.push({
+            id: last_item_id + 1,
+            firstName: firstName,
+            secondName: secondName,
+            phone: phone,
+            email: email,
+            userType: userType,
+            password: password,
+        });
+        let writeData = fs.writeFile(
+            "./users.json",
+            JSON.stringify(data),
+            (err, result) => {
+                if (err) {
+                    const status = 401;
+                    const message = err;
+                    res.status(status).json({ status, message });
+                    return;
+                }
+            }
+        );
+    });
+    const access_token = createToken({ email, password });
+    res.status(200).json({ access_token });
+});
+
+// Vendor Login section
+server.post("/api/auth/login/guest", (req, res) => {
+    const { email, password } = req.body;
+
+    // Checking if email and password are correct
+    if (!isGuestLoginAuthenticated({ email, password })) {
+        const status = 401;
+        const message = "Incorrect email or password";
+        res.status(status).json({ status, message });
+        return;
+    }
+    const access_token = createToken({ email, password });
+    res.status(200).json({ access_token });
+});
+
+
+
+server.use(router);
 // Server running
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`);
